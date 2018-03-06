78189b6aa951efd8db15949295befaad41aefa11
Mikele11 <mihailo_rlyash@mail.ru>
2018-03-06 17:39:53 +0200

    first commit

---
+++ app/home/index.controller.js
@@ -0,0 +1,216 @@
+﻿
+(function () {
+    'use strict';
+		var myApp = angular.module('app', []);
+		myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+			console.log("I am in controller");
+			$scope.filteredTodos = []
+		   ,$scope.currentPage = 1
+		   ,$scope.numPerPage = 4
+		   ,$scope.maxSize = 5;
+
+		   
+		var refresh = function() {
+		  $http.get('/workers').success(function(response) {
+			console.log("Geter");
+			$scope.workers = response;
+			$scope.len=$scope.workers.length;
+			$scope.page=Math.ceil($scope.len/$scope.numPerPage);
+			$scope.contact = "";
+		  });
+		};
+		refresh();
+		//панинация
+		  $scope.numPages = function () {
+			$http.get('/workers').success(function(response) {
+			$scope.workers = response;
+			$scope.numPages= Math.ceil($scope.workers.length / $scope.numPerPage);
+		   });  
+		  };
+		var pagin = function() {
+		  $scope.$watch('currentPage + numPerPage', function() {
+			$http.get('/workers').success(function(response) {
+			var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+			var end = begin + $scope.numPerPage; 
+			$scope.filteredTodos = $scope.workers.slice(begin, end);
+			}); 
+		  });
+		};
+		pagin();
+		//конец пагинации
+		$scope.addContact = function() {
+			if (($('.form-control.fname').val()=='')||($('.form-control.lname').val()=='')||($('.form-control.position').val()=='')){
+				alert('Enter value');
+			}else{
+			  console.log($scope.contact);
+			  $http.post('/workers', $scope.contact).success(function(response) {
+				console.log(response);
+				pagin();			  
+				refresh();
+			  });
+			}
+		};
+		
+		$scope.getAll = function() {
+			refresh();
+			pagin();			  
+		};
+				
+		$scope.remove = function(id) {
+		  console.log(id);
+		  $http.delete('/workers/' + id).success(function(response) {
+			pagin();
+			refresh();
+		  });
+		};
+		$scope.deleteSelected = function(id) {
+		  $http.get('/workers').success(function(response) {
+			$scope.workers = response;
+			$( ".class1" ).each(function( index ) {
+				if ($(this).prop('checked')==true){
+					var myID=$scope.workers[index]._id;
+					$http.delete('/workers/' + myID).success(function(response) {
+						pagin();
+						refresh();
+					});
+				};
+			});
+		  });
+		};
+
+		$scope.edit = function(id) {
+		  console.log(id);
+		  $http.get('/workers/' + id).success(function(response) {
+			$scope.contact = response;
+		  });
+		};  		
+		$scope.update = function() {
+		  console.log($scope.contact._id);
+		  $http.put('/workers/' + $scope.contact._id, $scope.contact).success(function(response) {
+			pagin(); 
+			refresh();
+		  })
+		};
+
+		$scope.deselect = function() {
+		  $scope.contact = "";
+		}
+
+		//----------------------------------------------		
+		jQuery(document).ready(function($) {
+			$('.table>div:nth-of-type(1)>div:nth-of-type(1)').click(function(){
+				var fname = $('.form-control:nth-of-type(1)').val();
+							
+				function buildUrl(url, parameters){
+				  var qs = "";
+				  for(var key in parameters) {
+					var value = parameters[key];
+					qs += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+				  }
+				  if (qs.length > 0){
+					qs = qs.substring(0, qs.length-1);
+					url = url + "?" + qs;
+				  }
+				  return url;
+				}
+				
+				var url = "/fname/";
+				var parameters = {
+				  fname: fname
+				};
+				console.log(buildUrl(url, parameters));
+				var urlsend = buildUrl(url, parameters);
+				$.ajax({
+				  url: urlsend,
+				  type: "GET",
+				  data: {"fname":fname},
+				  cache: false,
+				  success: function(response){
+					console.log('My filter'); 
+					$scope.filteredTodos=response;
+					console.log($scope.filteredTodos);
+					console.log('end of me filter');
+					$('#firstN').trigger('click');
+				   }
+				});				
+			});
+			//--------------------------------- end fname
+			$('.table>div:nth-of-type(1)>div:nth-of-type(2)').click(function(){
+				var lname = $('.form-control.lname').val();
+				console.log(lname);		
+				function buildUrl(url, parameters){
+				  var qs = "";
+				  for(var key in parameters) {
+					var value = parameters[key];
+					qs += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+				  }
+				  if (qs.length > 0){
+					qs = qs.substring(0, qs.length-1);
+					url = url + "?" + qs;
+				  }
+				  return url;
+				}
+				
+				var url = "/lname/";
+				var parameters = {
+				  lname: lname
+				};
+				console.log(buildUrl(url, parameters));
+				var urlsend = buildUrl(url, parameters);
+				$.ajax({
+				  url: urlsend,
+				  type: "GET",
+				  data: {"lname":lname},
+				  cache: false,
+				  success: function(response){
+					console.log('My filter'); 
+					$scope.filteredTodos=response;
+					console.log($scope.filteredTodos);
+					console.log('end of me filter');
+					$('#lastN').trigger('click');
+				   }
+				});				
+			});
+			//------------------------------------end lname
+			$('.table>div:nth-of-type(1)>div:nth-of-type(3)').click(function(){
+				var position = $('.form-control.position').val();
+							
+				function buildUrl(url, parameters){
+				  var qs = "";
+				  for(var key in parameters) {
+					var value = parameters[key];
+					qs += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+				  }
+				  if (qs.length > 0){
+					qs = qs.substring(0, qs.length-1);
+					url = url + "?" + qs;
+				  }
+				  return url;
+				}
+				
+				var url = "/position/";
+				var parameters = {
+				  position: position
+				};
+				console.log(buildUrl(url, parameters));
+				var urlsend = buildUrl(url, parameters);
+				$.ajax({
+				  url: urlsend,
+				  type: "GET",
+				  data: {"position":position},
+				  cache: false,
+				  success: function(response){
+					console.log('My filter'); 
+					$scope.filteredTodos=response;
+					console.log($scope.filteredTodos);
+					console.log('end of me filter');
+					$('#positionN').trigger('click');
+				   }
+				});				
+			});			
+			
+		});//ready
+		//----------------------------------------------
+		
+	}]);﻿
+})();
---
+++ server.js
@@ -0,0 +1,115 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');//'/app'
+});
+app.get('/app', function (req, res) {
+    return res.redirect('/workers');//'/app'
+});
+
+//......................................................old server
+
+var mongojs = require('mongojs');
+var db = mongojs('workers', ['workers']);
+var bodyParser = require('body-parser');
+
+app.get('/workers', function (req, res) {
+  console.log('All right');
+
+  db.workers.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  })
+});
+
+app.post('/workers', function (req, res) {
+  console.log(req.body);
+  db.workers.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/workers/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.workers.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/workers/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.workers.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+//-------------------------------------------------------------------
+app.get('/fname/', function (req, res) {
+  var fname1 = req.query.fname[0];
+  console.log(req.query.fname[0]);
+  db.workers.find({fname: fname1}, function(err,doc){
+        if (err) throw err;
+        console.log(doc);
+		res.json(doc);
+    })
+});
+app.get('/lname/', function (req, res) {
+  var lname1 = req.query.lname[0];
+  console.log(req.query);
+  db.workers.find({lname: lname1}, function(err,doc){
+        if (err) throw err;
+        console.log(doc);
+		res.json(doc);
+    })
+});
+app.get('/position/', function (req, res) {
+  var position1 = req.query.position[0];
+  console.log(position1); 
+  db.workers.find({position: position1}, function(err,doc){
+        if (err) throw err;
+        console.log(doc);
+		res.json(doc);
+    })
+});
+//-------------------------------------------------------------------
+
+app.put('/workers/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.workers.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {fname: req.body.fname, lname: req.body.lname, position: req.body.position}},//change avtomatic change
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+//..............old server cotinue
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
